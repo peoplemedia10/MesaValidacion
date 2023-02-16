1597669a91285976bae1fa87d55b450c890f8b4f
People <alfredtapia1999@outlook.com>
2023-02-16 16:52:43 -0600

    Cambios en maquetación

--- src/app/components/home/modal-foro/modal-foro.component.ts
+++ src/app/components/home/modal-foro/modal-foro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ArchivosService } from 'src/app/servicios/archivos.service';
 import { ForoService } from 'src/app/servicios/foro.service';
@@ -8,8 +8,9 @@ import { ForoService } from 'src/app/servicios/foro.service';
   templateUrl: './modal-foro.component.html',
   styleUrls: ['./modal-foro.component.scss']
 })
-export class ModalForoComponent implements OnInit {
+export class ModalForoComponent implements OnInit, AfterViewInit {
   panelOpenState = false
+  @ViewChild('list') list?: ElementRef<HTMLDivElement>;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public objeto: any,
@@ -20,6 +21,11 @@ export class ModalForoComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  ngAfterViewInit() {
+    const maxScroll = this.list?.nativeElement.scrollHeight;
+    this.list?.nativeElement.scrollTo({ top: maxScroll, behavior: 'smooth' });
   }
 
 }
